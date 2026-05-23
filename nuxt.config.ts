8e2d14f6fe5336d1cad0513d14d61d14fafdb9cd
Colin <colinclissonhg@gmail.com>
2026-05-23 19:32:11 -0400

    feat: setup effect

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,34 @@
 import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const effectLanguageServicePlugin = {
+  name: "@effect/language-service",
+} as const;
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+  typescript: {
+    tsConfig: {
+      compilerOptions: {
+        plugins: [effectLanguageServicePlugin],
+        exactOptionalPropertyTypes: true,
+        noUnusedLocals: true,
+      },
+    },
+  },
+  nitro: {
+    typescript: {
+      tsConfig: {
+        compilerOptions: {
+          plugins: [effectLanguageServicePlugin],
+          exactOptionalPropertyTypes: true,
+          noUnusedLocals: true,
+        },
+      },
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
