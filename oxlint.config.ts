a36939a7a5d739da701d1c95b46e5efda1a15f48
Colin <colinclissonhg@gmail.com>
2026-05-30 11:19:13 -0400

    feat: enrich oxlint config

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from "oxlint";
 
 export default defineConfig({
+  plugins: [
+    "eslint",
+    "typescript",
+    "unicorn",
+    "oxc",
+    "import",
+    "vue",
+    "promise",
+  ],
   categories: {
-    correctness: "warn",
+    correctness: "error",
+    suspicious: "warn",
+    perf: "warn",
   },
   rules: {
     "eslint/no-unused-vars": "error",
+    "eslint/prefer-const": "error",
+    "eslint/no-console": "warn",
+    "promise/no-return-wrap": "error",
   },
 });
